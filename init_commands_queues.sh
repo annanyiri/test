76d0381976d191b5d2ff3e254dc26f19e4cf2b27
annanyiri <annaemesenyiri@gmail.com>
2024-10-21 07:53:13 +0200

    remove unused files

--- init_commands_queues.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-ethtool -L red0 tx 12 rx 12
-sudo tc qdisc add dev red0 root mqprio num_tc 12 queues 1@0 1@1 1@2 1@3 1@4 1@5 1@6 1@7 1@8 1@9 1@10 1@11 map 0 1 2 3 4 5 6 7 8 9 10 11 hw 0
-
