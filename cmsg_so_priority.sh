b82b1e3391063d3b8efb9a3af9b2a7eb1238e96b
Anna Emese Nyiri <annaemesenyiri@gmail.com>
2024-12-06 21:02:11 +0100

    skip test when jq is not installed, add nodad

--- cmsg_so_priority.sh
+++ cmsg_so_priority.sh
@@ -3,24 +3,22 @@
 
 source lib.sh
 
+readonly KSFT_SKIP=4
+
 IP4=192.0.2.1/24
-TGT4_NO_MASK=192.0.2.2
-TGT4="${TGT4_NO_MASK}/24"
-TGT4_RAW_NO_MASK=192.0.2.3
-TGT4_RAW="${TGT4_RAW_NO_MASK}/24"
+TGT4=192.0.2.2
+TGT4_RAW=192.0.2.3
 IP6=2001:db8::1/64
-TGT6_NO_MASK=2001:db8::2
-TGT6="${TGT6_NO_MASK}/64"
-TGT6_RAW_NO_MASK=2001:db8::3
-TGT6_RAW="${TGT6_RAW_NO_MASK}/64"
+TGT6=2001:db8::2
+TGT6_RAW=2001:db8::3
 PORT=1234
 DELAY=4000
 TOTAL_TESTS=0
 FAILED_TESTS=0
 
 if ! command -v jq &> /dev/null; then
-    echo "Error: jq is not installed." >&2
-    exit 1
+    echo "SKIP cmsg_so_priroity.sh test: jq is not installed." >&2 
+    exit "$KSFT_SKIP"
 fi
 
 check_result() {
@@ -69,17 +67,17 @@ ip -n $NS link add link dummy1 name dummy1.10 up type vlan id 10 \
     egress-qos-map 0:0 1:1 2:2 3:3 4:4 5:5 6:6 7:7
 
 ip -n $NS address add $IP4 dev dummy1.10
-ip -n $NS address add $IP6 dev dummy1.10
+ip -n $NS address add $IP6 dev dummy1.10 nodad
 
 ip netns exec $NS sysctl -wq net.ipv4.ping_group_range='0 2147483647'
 
-ip -n $NS neigh add $TGT4_NO_MASK lladdr 00:11:22:33:44:55 nud permanent \
+ip -n $NS neigh add $TGT4 lladdr 00:11:22:33:44:55 nud permanent \
     dev dummy1.10
-ip -n $NS neigh add $TGT6_NO_MASK lladdr 00:11:22:33:44:55 nud permanent \
+ip -n $NS neigh add $TGT6 lladdr 00:11:22:33:44:55 nud permanent \
     dev dummy1.10
-ip -n $NS neigh add $TGT4_RAW_NO_MASK lladdr 00:11:22:33:44:66 nud permanent \
+ip -n $NS neigh add $TGT4_RAW lladdr 00:11:22:33:44:66 nud permanent \
     dev dummy1.10
-ip -n $NS neigh add $TGT6_RAW_NO_MASK lladdr 00:11:22:33:44:66 nud permanent \
+ip -n $NS neigh add $TGT6_RAW lladdr 00:11:22:33:44:66 nud permanent \
     dev dummy1.10
 
 tc -n $NS qdisc add dev dummy1 clsact
@@ -91,13 +89,13 @@ for i in 4 6; do
         echo "Test IPV$i, prot: $proto"
         for priority in {0..7}; do
             if [[ $i == 4 && $proto == "r" ]]; then
-                TGT=$TGT4_RAW_NO_MASK
+                TGT=$TGT4_RAW
             elif [[ $i == 6 && $proto == "r" ]]; then
-                TGT=$TGT6_RAW_NO_MASK
+                TGT=$TGT6_RAW
             elif [ $i == 4 ]; then
-                TGT=$TGT4_NO_MASK
+                TGT=$TGT4
             else
-                TGT=$TGT6_NO_MASK
+                TGT=$TGT6
             fi
 
             handle="${FILTER_COUNTER}${priority}"
@@ -115,7 +113,9 @@ for i in 4 6; do
                 check_result 1
             fi
 
-            ip netns exec $NS ./cmsg_sender -$i -Q $priority -d "${DELAY}" -p $proto $TGT $PORT
+            ip netns exec $NS ./cmsg_sender -$i -Q $priority -d "${DELAY}" \
+	            -p $proto $TGT $PORT
+
             pkts=$(tc -n $NS -j -s filter show dev dummy1 egress \
                 | jq ".[] | select(.options.handle == ${handle}) | \
                 .options.actions[0].stats.packets")
@@ -126,7 +126,9 @@ for i in 4 6; do
                 check_result 1
             fi
 
-            ip netns exec $NS ./cmsg_sender -$i -P $priority -d "${DELAY}" -p $proto $TGT $PORT
+            ip netns exec $NS ./cmsg_sender -$i -P $priority -d "${DELAY}" \
+	            -p $proto $TGT $PORT
+
             pkts=$(tc -n $NS -j -s filter show dev dummy1 egress \
                 | jq ".[] | select(.options.handle == ${handle}) | \
                 .options.actions[0].stats.packets")
